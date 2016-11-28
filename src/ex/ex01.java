92b8593d4732b3afb54c4eeb03d53b19a3911eae
HASAKIBOOM <105021063@live.asia.edu.tw>
2016-11-28 16:03:18 +0800

    up

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -5,14 +5,32 @@
 	Random randnum = new Random();
 	randnum.setSeed(23323456)
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021063 彭明捷
  */
-
+import java.util.Scanner;
+import java.util.Random;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random rnd=new Random(42);
+		Scanner scn=new Scanner(System.in); 
+		int data[]=new int[6];
+		int n=scn.nextInt();
+		rnd.setSeed(n);
+		for(int i=0;i<6;i++){
+		data[i]=rnd.nextInt(42)+1;
+		for(int j=0;j<i;j++){
+			if(data[i]==data[j]){
+				data[i]=rnd.nextInt(42)+1;
+			}
+		}
+			
+		}
+		for(int i=0;i<data.length;i++){
+			System.out.print(data[i]+" ");
+			
+		}
+			}
 
-	}
-
-}
+		}
